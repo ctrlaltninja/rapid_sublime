0b39889064809d032c4abd38f37f0d8dccf23bd2
Jussi Sammaltupa <jussi@ctrlaltninja.com>
2018-01-04 12:35:33 +0200

    Debugger: Support for toggling and clearing breakpoints.

--- rapid_debug.py
+++ rapid_debug.py
@@ -1,14 +1,9 @@
 import sublime, sublime_plugin
-import time
-import re
-
-from .rapid_output import RapidOutputView
 from .rapid_utils import open_file_location
 from .rapid_utils import escape_filename
 from .rapid import RapidConnectionThread
 
-# to run execute from the console:
-# view.run_command('rapid_eval')
+REGION_KEY = "breakpoint"
 
 class RapidDebugStepCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
@@ -16,24 +11,45 @@ class RapidDebugStepCommand(sublime_plugin.TextCommand):
 
 class RapidDebugToggleBreakpoint(sublime_plugin.TextCommand):
 	def run(self, edit):
-		# TODO: remove breakpoints
-		regions = [s for s in self.view.sel()]
+		# BUG the breakpoint location is dependent on the column -> fudge selections to start from the
+		# beginning of the line for toggled points
+		previous_points = [r.begin() for r in self.view.get_regions(REGION_KEY)]
+		toggled_points = [r.begin() for r in self.view.sel()]
+
+		new_points = [p for p in toggled_points if not p in previous_points]
+		removed_points = [p for p in toggled_points if p in previous_points]
+		current_points = list(set().union(previous_points, new_points).difference(removed_points))
+
+		regions = [sublime.Region(point, point) for point in current_points]
+
+		self.view.add_regions(REGION_KEY, regions, "mark", "dot", sublime.HIDDEN | sublime.PERSISTENT)
+
 		filename = self.view.file_name()
 
-		for region in regions:
-			row,_ = self.view.rowcol(region.begin())
+		# call add breakpoint for all valid breakpoints
+		for point in new_points:
+			row,_ = self.view.rowcol(point)
 			self.setBreakpoint(filename, row)
 
-		self.view.add_regions("breakpoint", regions, "mark", "dot", sublime.HIDDEN | sublime.PERSISTENT)
+		# call remove breakpoint for all old breakpoints
+		for point in removed_points:
+			row,_ = self.view.rowcol(point)
+			self.removeBreakpoint(filename, row)
 
 	def setBreakpoint(self, filename, row):
 		filename = escape_filename(filename)
 		message = "\nDebug.addBreakpoint(\"%(file)s\", %(row)d)" % { 'file': filename, 'row': row }
 		RapidConnectionThread.sendString(message)
 
+	def removeBreakpoint(self, filename, row):
+		filename = escape_filename(filename)
+		message = "\nDebug.removeBreakpoint(\"%(file)s\", %(row)d)" % { 'file': filename, 'row': row }
+		RapidConnectionThread.sendString(message)
+
 class RapidDebugRemoveAllBreakpoints(sublime_plugin.TextCommand):
 	def run(self, edit):
-		self.view.erase_regions("breakpoint")
+		RapidConnectionThread.sendString("\nDebug.removeAllBreakpoints()")
+		self.view.erase_regions(REGION_KEY)
 
 def parseDebugMessage(cmd):
 	matches = re.match('#ATLINE (.*):(.*)', cmd)
@@ -48,4 +64,3 @@ def parseDebugMessage(cmd):
 		region = [s for s in view.sel()]
 		icon = "Packages/rapid_sublime/icons/current_line.png"
 		view.add_regions("current", region, "mark", icon, sublime.HIDDEN)
-	return
